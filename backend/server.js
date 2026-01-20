a3de018b6c70dd43ae6ce787c68a9d6f0ae7c9c7
Sanjana-byte-hub <sanjana.gupta557817@gmail.com>
2026-01-20 16:08:56 +0530

    Fix route file separation

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,13 @@
+
+require("dotenv").config();
+
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
 
 const PORT = process.env.PORT || 3000;
 
-// Connect to MongoDB
-connectDB()
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => {
-    console.error("DB connection error:", err);
-    process.exit(1);
-  });
+connectDB();
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
