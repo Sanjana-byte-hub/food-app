4b274e44c1fcfcf81d7d2acba9443e1573f3e55a
Sanjana-byte-hub <sanjana.gupta557817@gmail.com>
2026-01-20 10:18:32 +0530

    Fix Express v5 CORS wildcard crash

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
